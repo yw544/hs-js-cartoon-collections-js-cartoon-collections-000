519bfada8f0b074895906783e986e4873f2e0db6
York Wang <yw544@nyu.edu>
2017-12-04 16:53:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
-function dwarfRollCall(dwarves) {
+function dwarfRollCall(dwarves)
+{
+var dwarf = [];
+
+for (var i = 0; i < dwarf.length; i ++)
+{dwarf.push(i + 1 + ". " + dwarf[i] + " ")}
+
+return dwarf.join;
 }
 
 function summonCaptainPlanet(planeteerCalls){
